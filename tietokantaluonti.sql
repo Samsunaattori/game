d477b77611dae349a2f504102254002d118f0b49
alisawpi <32746949+alisawpi@users.noreply.github.com>
2017-11-21 10:37:23 +0200

    db2

--- tietokantaluonti.sql
+++ tietokantaluonti.sql
@@ -2,7 +2,19 @@ CREATE DATABASE omapeli;
 use omapeli; 
 CREATE TABLE Room(
 PositionID INT(12) NOT NULL, 
-Nimi VARCHAR(20), 
-RoomDescr VARCHAR(20,
+RoomN VARCHAR(20), 
+RoomDescr VARCHAR(20),
 PRIMARY KEY (PositionID));
+
+CREATE TABLE Player(
+PositionID INT(12) NOT NULL, 
+PlayerN VARCHAR(20), 
+FOREIGN KEY (PositionID) REFERENCES TO Room(PositionID)); 
+
+CREATE TABLE NPC(
+NPCID INT(12), 
+isAlive INT(5), 
+NPCN VARCHAR(20), 
+PositionID INT(12), 
+FOREIGN KEY (PositionID) REFERENCES TO Room(PositionID)); 
 
