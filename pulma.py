d5d871da43bb599f671d33f257de5b0a625ac79e
alisawpi <32746949+alisawpi@users.noreply.github.com>
2017-11-23 09:13:58 +0200

    sisennys

--- pulma.py
+++ pulma.py
@@ -1,126 +1,128 @@
-a1=a2=a3=a4=b1=b2=b3=b4=c1=c2=c3=c4=d1=d2=d3=d4=0
+
 
 def muunna(i):
     return (i+1)%2
+def puzzle():
+    a1=a2=a3=a4=b1=b2=b3=b4=c1=c2=c3=c4=d1=d2=d3=d4=0
+    while not (a1==a2==a3==a4==b1==b4==c1==c4==d1==d2==d3==d4==0 and b2==b3==c2==c3==1):
+        print("  "+str(1)+" "+str(2)+" "+str(3)+" "+str(4))
+        print("A "+str(a1)+" "+str(a2)+" "+str(a3)+" "+str(a4))
+        print("B "+str(b1)+" "+str(b2)+" "+str(b3)+" "+str(b4))
+        print("C "+str(c1)+" "+str(c2)+" "+str(c3)+" "+str(c4))
+        print("D "+str(d1)+" "+str(d2)+" "+str(d3)+" "+str(d4))
+
+        ruutu = input("Anna ruutu: ")
+
+        if ruutu == "A1" or ruutu == "a1":
+            a1 = muunna(a1)
+            a2 = muunna(a2)
+            b1 = muunna(b1)
+
+        elif ruutu == "A2" or ruutu == "a2":
+            a1 = muunna(a1)
+            a2 = muunna(a2)
+            a3 = muunna(a3)
+            b2 = muunna(b2)
+
+        elif ruutu == "A3" or ruutu == "a3":
+            a2 = muunna(a2)
+            a3 = muunna(a3)
+            a4 = muunna(a4)
+            b3 = muunna(b3)
+
+        elif ruutu == "A4" or ruutu == "a4":
+            a3 = muunna(a3)
+            a4 = muunna(a4)
+            b4 = muunna(b4)
+
+        elif ruutu == "B1" or ruutu == "b1":
+            a1 = muunna(a1)
+            b1 = muunna(b1)
+            b2 = muunna(b2)
+            c1 = muunna(c1)
+
+        elif ruutu == "B2" or ruutu == "b2":
+            a2 = muunna(a2)
+            b1 = muunna(b1)
+            b2 = muunna(b2)
+            b3 = muunna(b3)
+            c2 = muunna(c2)
+
+        elif ruutu == "B3" or ruutu == "b3":
+            a3 = muunna(a3)
+            b2 = muunna(b2)
+            b3 = muunna(b3)
+            b4 = muunna(b4)
+            c3 = muunna(c3)
+
+        elif ruutu == "B4" or ruutu == "b4":
+            a4 = muunna(a4)
+            b3 = muunna(b3)
+            b4 = muunna(b4)
+            c4 = muunna(c4)
+
+        elif ruutu == "C1" or ruutu == "c1":
+            b1 = muunna(b1)
+            c1 = muunna(c1)
+            c2 = muunna(c2)
+            d1 = muunna(d1)
+
+        elif ruutu == "C2" or ruutu == "c2":
+            b2 = muunna(b2)
+            c1 = muunna(c1)
+            c2 = muunna(c2)
+            c3 = muunna(c3)
+            d2 = muunna(d2)
+
+        elif ruutu == "C3" or ruutu == "c3":
+            b3 = muunna(b3)
+            c2 = muunna(c2)
+            c3 = muunna(c3)
+            c4 = muunna(c4)
+            d3 = muunna(d3)
+
+        elif ruutu == "C4" or ruutu == "c4":
+            b4 = muunna(b4)
+            c3 = muunna(c3)
+            c4 = muunna(c4)
+            d4 = muunna(d4)
+
+        elif ruutu == "D1" or ruutu == "d1":
+            c1 = muunna(c1)
+            d1 = muunna(d1)
+            d2 = muunna(d2)
+
+        elif ruutu == "D2" or ruutu == "d2":
+            c2 = muunna(c2)
+            d1 = muunna(d1)
+            d2 = muunna(d2)
+            d3 = muunna(d3)
+
+        elif ruutu == "D3" or ruutu == "d3":
+            c3 = muunna(c3)
+            d2 = muunna(d2)
+            d3 = muunna(d3)
+            d4 = muunna(d4)
+
+        elif ruutu == "D4" or ruutu == "d4":
+            c4 = muunna(c4)
+            d3 = muunna(d3)
+            d4 = muunna(d4)
+
+        elif ruutu == "reset":
+            a1=a2=a3=a4=b1=b2=b3=b4=c1=c2=c3=c4=d1=d2=d3=d4=0
+        
+        else:
+            print("virheellinen komento")
 
 
-while not (a1==a2==a3==a4==b1==b4==c1==c4==d1==d2==d3==d4==0 and b2==b3==c2==c3==1):
+
     print("  "+str(1)+" "+str(2)+" "+str(3)+" "+str(4))
     print("A "+str(a1)+" "+str(a2)+" "+str(a3)+" "+str(a4))
     print("B "+str(b1)+" "+str(b2)+" "+str(b3)+" "+str(b4))
     print("C "+str(c1)+" "+str(c2)+" "+str(c3)+" "+str(c4))
     print("D "+str(d1)+" "+str(d2)+" "+str(d3)+" "+str(d4))
 
-    ruutu = input("Anna ruutu: ")
+    print("The door opens")
 
-    if ruutu == "A1" or ruutu == "a1":
-        a1 = muunna(a1)
-        a2 = muunna(a2)
-        b1 = muunna(b1)
-
-    elif ruutu == "A2" or ruutu == "a2":
-        a1 = muunna(a1)
-        a2 = muunna(a2)
-        a3 = muunna(a3)
-        b2 = muunna(b2)
-
-    elif ruutu == "A3" or ruutu == "a3":
-        a2 = muunna(a2)
-        a3 = muunna(a3)
-        a4 = muunna(a4)
-        b3 = muunna(b3)
-
-    elif ruutu == "A4" or ruutu == "a4":
-        a3 = muunna(a3)
-        a4 = muunna(a4)
-        b4 = muunna(b4)
-
-    elif ruutu == "B1" or ruutu == "b1":
-        a1 = muunna(a1)
-        b1 = muunna(b1)
-        b2 = muunna(b2)
-        c1 = muunna(c1)
-
-    elif ruutu == "B2" or ruutu == "b2":
-        a2 = muunna(a2)
-        b1 = muunna(b1)
-        b2 = muunna(b2)
-        b3 = muunna(b3)
-        c2 = muunna(c2)
-
-    elif ruutu == "B3" or ruutu == "b3":
-        a3 = muunna(a3)
-        b2 = muunna(b2)
-        b3 = muunna(b3)
-        b4 = muunna(b4)
-        c3 = muunna(c3)
-
-    elif ruutu == "B4" or ruutu == "b4":
-        a4 = muunna(a4)
-        b3 = muunna(b3)
-        b4 = muunna(b4)
-        c4 = muunna(c4)
-
-    elif ruutu == "C1" or ruutu == "c1":
-        b1 = muunna(b1)
-        c1 = muunna(c1)
-        c2 = muunna(c2)
-        d1 = muunna(d1)
-
-    elif ruutu == "C2" or ruutu == "c2":
-        b2 = muunna(b2)
-        c1 = muunna(c1)
-        c2 = muunna(c2)
-        c3 = muunna(c3)
-        d2 = muunna(d2)
-
-    elif ruutu == "C3" or ruutu == "c3":
-        b3 = muunna(b3)
-        c2 = muunna(c2)
-        c3 = muunna(c3)
-        c4 = muunna(c4)
-        d3 = muunna(d3)
-
-    elif ruutu == "C4" or ruutu == "c4":
-        b4 = muunna(b4)
-        c3 = muunna(c3)
-        c4 = muunna(c4)
-        d4 = muunna(d4)
-
-    elif ruutu == "D1" or ruutu == "d1":
-        c1 = muunna(c1)
-        d1 = muunna(d1)
-        d2 = muunna(d2)
-
-    elif ruutu == "D2" or ruutu == "d2":
-        c2 = muunna(c2)
-        d1 = muunna(d1)
-        d2 = muunna(d2)
-        d3 = muunna(d3)
-
-    elif ruutu == "D3" or ruutu == "d3":
-        c3 = muunna(c3)
-        d2 = muunna(d2)
-        d3 = muunna(d3)
-        d4 = muunna(d4)
-
-    elif ruutu == "D4" or ruutu == "d4":
-        c4 = muunna(c4)
-        d3 = muunna(d3)
-        d4 = muunna(d4)
-
-    elif ruutu == "reset":
-        a1=a2=a3=a4=b1=b2=b3=b4=c1=c2=c3=c4=d1=d2=d3=d4=0
-    
-    else:
-        print("virheellinen komento")
-
-
-
-print("  "+str(1)+" "+str(2)+" "+str(3)+" "+str(4))
-print("A "+str(a1)+" "+str(a2)+" "+str(a3)+" "+str(a4))
-print("B "+str(b1)+" "+str(b2)+" "+str(b3)+" "+str(b4))
-print("C "+str(c1)+" "+str(c2)+" "+str(c3)+" "+str(c4))
-print("D "+str(d1)+" "+str(d2)+" "+str(d3)+" "+str(d4))
-
-print("The door opens")
+puzzle()
